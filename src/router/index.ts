8b7037ae7b495142e8d6570e9ef59ebf142d4b71
Cargo King <cargo.king@newchains.cn>
2025-10-17 15:40:34 +0800

    add about

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,11 @@ const router = createRouter({
         {
             path: '/',
             name: 'home',
-            component: () => import('@/views/HomeView.vue')
+            component: () => import('@/views/Home.vue')
+        },{
+            path: '/about',
+            name: 'about',
+            component: () => import('@/views/About.vue')
         }
     ]
 })
